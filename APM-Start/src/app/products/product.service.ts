d9ad64d7fd58f45463489213859dfc23d5138c69
Patricio Vargas <pevargas2730@gmail.com>
2019-09-09 00:48:39 -0400

    trasnforming product

--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, tap, map } from 'rxjs/operators';
 
 import { Product } from './product';
 import { Supplier } from '../suppliers/supplier';
@@ -15,6 +15,16 @@ export class ProductService {
   private productsUrl = 'api/products';
   private suppliersUrl = this.supplierService.suppliersUrl;
   products$ = this.http.get<Product[]>(this.productsUrl).pipe(
+    map(products =>
+      products.map(
+        product =>
+          ({
+            ...product,
+            price: product.price * 5.5,
+            searchKey: [product.productName]
+          } as Product)
+      )
+    ),
     tap(data => console.log('Products: ', JSON.stringify(data))),
     catchError(this.handleError)
   );
